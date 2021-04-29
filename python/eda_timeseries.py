5987467a451d7f77584b9a4209ac036dde386e07
brian546 <43593664+brian546@users.noreply.github.com>
2021-04-29 15:59:07 +0800

    update eda timeseries

---
+++ python/eda_timeseries.py
@@ -0,0 +1,62 @@
+# %%
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+# %%
+df = pd.read_csv('../data/test_timeseries/test_timeseries.csv',parse_dates=['date'])
+print(df.info())
+# %%
+print(df.isnull().sum())
+
+# %%
+
+# number of regions
+df['fips'].nunique()
+
+# %%
+
+# find out if any multicolinearity
+
+# T2M highly collerated (corr > 0.7) with QV2M,T2MDEW,T2MWET, T2M_MAX, T2M_MIN, and TS 
+
+# WS10M highly collerated  with W10M_MAX, WS10M_MIN, WS50M, WS550M_MAX, WS550M_MIN
+
+# columns left: PRECTOT, PS, T2M, T2M_RANGE, WS10M, WS50M_RANGE
+
+# plt.figure(figsize=(20,20))
+# sns.heatmap(df.drop(columns=['fips','date']).corr(),annot=True)
+
+# %%
+df['date'].head()
+
+# %%
+
+new_df = df[['fips','date', 'PRECTOT', 'PS', 'T2M', 'T2M_RANGE', 'WS10M', 'WS50M_RANGE','score']]
+
+# %%
+
+# proving that the dought score was released every Tuesday, reviewing the dought condtion in the past week
+
+print(new_df[new_df['date'].dt.weekday == 1]['score'].isnull().sum())
+
+# %%
+
+# filter out row at the date that is after the last Tuesday of 2020 and row at the first Tuesday.
+
+new_df = new_df[(df['date'] > pd.Timestamp(2019,1,1)) & (df['date'] < pd.Timestamp(2020,12,30))]
+
+# %%
+
+# get the mean of data per week 
+week_df = new_df.groupby('fips').resample('W-TUE', on='date').mean()
+
+print(week_df)
+# %%
+week_df['score'].isnull().sum()
+
+
+# %%
+
+# %%
